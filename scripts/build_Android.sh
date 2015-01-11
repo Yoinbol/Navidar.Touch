4069bb8da1a7af40d6a1483e7940d82f49bb3a23
Diego García <chinopb@gmail.com>
2015-01-11 14:20:44 -0600

    Sencha Touch project and build scripts
    -Sencha Touch project and build scripts

---
+++ scripts/build_Android.sh
@@ -0,0 +1,46 @@
+cd ..
+target="testing";
+projName=`cat sencha/app.json | grep "\"name\"" | awk -F\" '{print $4}'`
+
+echo "Building project: $projName"
+
+buildDir="sencha/build/$target/$projName"
+cordovaDir="cordova/www"
+
+echo $'\e[32m''building Sencha ' $'\e[00m'
+
+cd sencha/
+
+sencha app build $target
+RC=$?
+cd ..
+# isError=`grep ERROR /tmp/st2.out > /dev/null 2>&1`
+if [ $RC -ne 0 ]; then
+	echo $'\e[31m''Sencha build error' $'\e[00m'
+	cat /tmp/st2.out
+	cd ..
+	exit 1
+else 	
+	echo $'\e[32m''Sencha build DONE' $'\e[00m'
+
+	echo `pwd`
+	echo "Creating Cordova folder if not present."
+	mkdir -p $cordovaDir
+
+	echo "Erasing old contents from Cordova dir"
+	rm -rf $cordovaDir/* 
+
+	echo "Copying to Cordova..."
+	cp -Rf $buildDir/* $cordovaDir/
+
+	cd cordova
+
+	echo "Building Cordova Android..."
+	cordova build android
+
+	# Optional, clear out the build directory
+	rm -rf $buildDir
+
+	echo $'\e[32m''DONE!' $'\e[00m'
+	exit 0
+fi
---
+++ sencha/build/temp/production/Navidar/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\dgarcia\Documents\GitHub\Navidar.Touch\sencha\app.js
